e45d4aca1c45c58841cdc481f7b272ab9c3b9482
Manikanta <23eg107b46@anurag.edu.in>
2025-08-04 14:33:49 +0530

    first commit

---
+++ WPM Project/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('welcome to express');
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
